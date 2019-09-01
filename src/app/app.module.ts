bbbfdb0c8bc4423dd4ac068861756b008a4e1c1e
joseT <jmtm_2002@hotmail.com>
2019-09-01 17:57:52 -0430

    se quito el use hash true

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,7 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
   imports: [
     BrowserAnimationsModule,
     RouterModule.forRoot(AppRoutes,{
-      useHash: true
+      useHash: false
     }),
     SidebarModule,
     NavbarModule,
